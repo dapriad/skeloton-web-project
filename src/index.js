0128fe03fea0e43cd1218c493c16025cea488187
Daniel Pribeagu <desarrollo@tagingenieros.com>
2021-06-16 16:33:31 +0200

    Added css loader to project with hello class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
@@ -6,6 +7,7 @@ function component() {
     console.log('%cindex.js line:6 "Entra"', 'color: #007acc;', "Entra");
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
