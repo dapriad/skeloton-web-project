735764c8d6b8ea9874f530c9ed516c9ab46c6c63
Daniel Pribeagu <desarrollo@tagingenieros.com>
2021-06-16 16:27:07 +0200

    New name for compiled js and added css loader and styke loader for asset managment

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     mode: process.env.MODE,
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
 }
