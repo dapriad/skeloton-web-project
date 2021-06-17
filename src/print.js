0857ae2fa3d84ce5597885ad0c307f696f5d01f8
Daniel Pribeagu <desarrollo@tagingenieros.com>
2021-06-17 12:54:52 +0200

    Watch option added to package json

--- src/print.js
+++ src/print.js
@@ -1,3 +1,4 @@
 export default function printMe() {
     console.log('%cprint.js line:2 "I get called from print.js"', 'color: #007acc;', "I get called from print.js");
+    console.log('%cprint.js line:2 "I get called from print.js"', 'color: #007acc;', "I get called from print.js twice!");
 }
