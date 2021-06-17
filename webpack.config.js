e877c6add9bfc35838c9ddcdc16c5e45e7ac5b5b
Daniel Pribeagu <desarrollo@tagingenieros.com>
2021-06-17 16:44:34 +0200

    Caching webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: process.env.MODE,
-    entry: {
-        index: {
-            import: './src/index.js'
-        }
-    },
+    entry: './src/index.js',
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development'
+            title: 'Caching',
         })
     ],
     devtool: 'inline-source-map', // For debug mode
@@ -40,5 +36,18 @@ module.exports = {
                 }],
             },
         ],
-    }
+    },
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
 }
