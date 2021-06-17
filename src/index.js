f980fb4e6f52cc16930a8a7d7cec81408a110002
Daniel Pribeagu <desarrollo@tagingenieros.com>
2021-06-17 10:29:25 +0200

    Custom loaders for data (json5, toml and yaml)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ import Icon from './assets/img/github.png';
 import Data from './data/data.xml';
 import Notes from './data/data.csv';
 
+import toml from './data/data.toml';
+import yaml from './data/data.yaml';
+import json from './data/data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
+
 function component() {
     const element = document.createElement('div');
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@
 require('dotenv').config();
 
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
     mode: process.env.MODE,
@@ -41,6 +44,27 @@ module.exports = {
             {
                 test: /\.xml$/i,
                 use: ['xml-loader'],
+            },
+            {
+                test: /\.toml$/i,
+                type: 'json',
+                parser: {
+                    parse: toml.parse,
+                },
+            },
+            {
+                test: /\.yaml$/i,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                },
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
             },
         ],
     },
