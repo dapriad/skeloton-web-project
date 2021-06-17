4c960b11d013bd1b7e67ffecc1a53fc25ccdffe0
Daniel Pribeagu <desarrollo@tagingenieros.com>
2021-06-17 10:34:24 +0200

    Clean up for next lesson - Output Managment

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
 import _ from 'lodash';
-import './assets/css/main.css'; // with css rules enabled in webpack
-import './assets/scss/main.scss';
-import Icon from './assets/img/github.png';
-
-import Data from './data/data.xml';
-import Notes from './data/data.csv';
-
-import toml from './data/data.toml';
-import yaml from './data/data.yaml';
-import json from './data/data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
-
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    myIcon.classList.add('sm-icon');
-
-    element.appendChild(myIcon);
-
-    console.log('%cindex.js line:24 Data', 'color: #007acc;', Data);
-    console.log('%cindex.js line:25 Notes', 'color: #007acc;', Notes);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@
 require('dotenv').config();
 
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
 
 module.exports = {
     mode: process.env.MODE,
@@ -12,60 +9,5 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s?[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            // Prefer `dart-sass`
-                            implementation: require.resolve("sass"),
-                        }
-                    }
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader'],
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader'],
-            },
-            {
-                test: /\.toml$/i,
-                type: 'json',
-                parser: {
-                    parse: toml.parse,
-                },
-            },
-            {
-                test: /\.yaml$/i,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse,
-                },
-            },
-            {
-                test: /\.json5$/i,
-                type: 'json',
-                parser: {
-                    parse: json5.parse,
-                },
-            },
-        ],
     },
 }
