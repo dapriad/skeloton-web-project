9cb5ee1e420d0446a9566addaa9496fda49c2b9f
Daniel Pribeagu <desarrollo@tagingenieros.com>
2021-06-23 17:06:30 +0200

    Created TopNav component and added to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,10 @@
   <!-- App.vue -->
 
   <v-app>
-    <v-app-bar app>
-      <v-app-bar-nav-icon>
-        <img
-          src="./assets/Fakelogo.png"
-          alt="logo of adidas 3 stripes plus name"
-        />
-      </v-app-bar-nav-icon>
-      <v-toolbar-title>Adidas</v-toolbar-title>
-    </v-app-bar>
+    <TopNav />
 
-    <!-- Sizes your content based upon application components -->
     <v-main>
-      <!-- Provides the application the proper gutter -->
       <v-container fluid>
-        <!-- If using vue-router -->
         <router-view></router-view>
       </v-container>
     </v-main>
@@ -28,31 +17,17 @@
 </template>
 
 <script>
+import TopNav from "./components/TopNav.vue";
+
 export default {
-  data: () => ({
-    links: ["Dashboard", "Messages", "Profile", "Updates"],
-  }),
-  created() {
-    console.log(this.$vuetify.application); // 56
+  name: "app",
+  components: {
+    TopNav,
   },
 };
 </script>
 
 <style lang="sass" scoped>
-// styles/generic/_index.scss
-
-// Generic styling for bare HTML elements (like H1, A, etc.).
-// These come with default styling from the browser so that
-// we can redefine them here.
-@import './reset.scss'
-
-@import './animations.scss'
-
-@import './colors.scss'
-
-@import './elevation.scss'
-
-@import './transitions.scss'
 .title-logo
   height: 30px
 </style>
